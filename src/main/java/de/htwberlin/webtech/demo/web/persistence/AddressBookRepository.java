2b49ef62f509edb89d574021d0f78c20483e3871
asaadal-maamari <Fenstersteuerungber Support Asaad.al-Maamari@Student.HTW-Berlin.de>
2022-11-14 18:59:00 +0100

    mailStein_2

--- src/main/java/de/htwberlin/webtech/demo/web/persistence/AddressBookRepository.java
+++ src/main/java/de/htwberlin/webtech/demo/web/persistence/AddressBookRepository.java
@@ -1,9 +1,13 @@
 package de.htwberlin.webtech.demo.web.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+
+@Repository
 public interface AddressBookRepository extends JpaRepository<AddressBookEntity, Long> {
 
     List<AddressBookEntity> findAllByPersonId(Long id);
